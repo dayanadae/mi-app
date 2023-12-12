c5260644856f52e6fe41c7ed772ecad8e42acdba
dayanadae <147381066+dayanadae@users.noreply.github.com>
2023-12-12 06:37:16 +0100

    cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'></div>
+      <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCampp</h1>
+      <Testimonio
+        nombre='Emma Boston'
+        pais='Suecia'
+        imagen='emma'
+        cargo='Ingeniera de Software'
+        empresa='Spotify'
+        testimonio='La Ingeniería de Software es una de las ramas de las ciencias de la computación que estudia la creación de software confiable y de calidad, basándose en métodos y técnicas de ingeniería, y brindando soporte operacional y de mantenimiento. El campo de estudio de la ingeniería de software integra ciencias de la computación, ciencias aplicadas y las ciencias básicas en las cuales se encuentra apoyada la ingeniería.'      
+      />
     </div>
   );
 }
